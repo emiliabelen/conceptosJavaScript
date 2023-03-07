d448a3aaa1f4f6e149fe2f72008a132d2a5938d2
belen ramos <ramos.emiliabelen@gmail.com>
2023-03-07 20:16:23 -0300

    listo ejercicio 1

--- js/1-variables.js
+++ js/1-variables.js
@@ -1,1 +1,16 @@
-/* */
+/* Es un comentario e multiples lineas*/
+//es un comentario de 1 sola linea
+
+//crear o declarar una variable (var- let - const)
+
+let producto = "ipad pro";
+let precio = 700;
+let luzEncendida = false;
+
+//mostrar datos en el documento (pag en blanco)
+document.write("hola mundo");
+document.write("<br>");
+document.write(precio * 380 );
+
+//mostrar en consola
+console.log(precio * 380)
