1cdab7e1cc5b72d6d1234ae6db166e18f48a4d5d
belen ramos <ramos.emiliabelen@gmail.com>
2023-03-09 20:30:53 -0300

    agregamos is nan

--- js/2-condicionales.js
+++ js/2-condicionales.js
@@ -12,11 +12,20 @@
 // 18 a 67 inclusive estan obligados
 
 let edad = parseInt(prompt("Ingrese su edad"));
+console.log(edad);
+console.log(isNaN(edad));
+//si no es un numero es verdadero=true
+//!(true)= false
+//!(false)= true
 
-if ((edad >= 16 && edad < 18) || edad > 67) {
-  document.write("Es opcional votar");
-} else if (edad >= 18 && edad <= 67) {
+if (!isNaN(edad))
+  if ((edad >= 16 && edad < 18) || edad > 67) {
+    document.write("Es opcional votar");
+  } else if (edad >= 18 && edad <= 67) {
     document.write("Debe votar");
   } else {
     document.write("No puede votar");
-  } 
+  }
+else {
+  document.write("Esto no es un numero");
+}
