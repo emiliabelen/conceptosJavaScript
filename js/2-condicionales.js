406da5882660fde654f12232b448822749909e0d
belen ramos <ramos.emiliabelen@gmail.com>
2023-03-09 20:01:22 -0300

    agregamos una condicion para evaluar cuando podemos votar y cunado es opcional

---
+++ js/2-condicionales.js
@@ -0,0 +1,22 @@
+//Estructura if-else
+
+/*if (condicion=== true)
+...{
+
+} else {
+    ...
+}*/
+
+//silocitar la edad del usuario e informar  si puede votar
+// 68 en adelante es opcional, 16 a 17 es opcional
+// 18 a 67 inclusive estan obligados
+
+let edad = parseInt(prompt("Ingrese su edad"));
+
+if ((edad >= 16 && edad < 18) || edad > 67) {
+  document.write("Es opcional votar");
+} else if (edad >= 18 && edad <= 67) {
+    document.write("Debe votar");
+  } else {
+    document.write("No puede votar");
+  } 
