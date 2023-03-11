3ffb994d87fecaa877819565e8fc07b22a3e4d9a
belen ramos <ramos.emiliabelen@gmail.com>
2023-03-11 16:32:12 -0300

    agrego switch

---
+++ js/3-switch.js
@@ -0,0 +1,26 @@
+// switch se utiliza en casos de menues
+/* en switch lo que va dentro del ( no es una condicion logica sino el contenido de una variable )
+si agrego texto en el case tiene q ser con ''
+
+switch(opcion){
+    case 1:
+        todas las lineas de codigo que quiero
+        ejecutar si la opcion del us es 1
+        break;
+
+        case 2:
+        todas las lineas de codigo que quiero
+        ejecutar si la opcion del us es 1
+        break;
+
+        case 3:
+        todas las lineas de codigo que quiero
+        ejecutar si la opcion del us es 1
+        break;;
+
+
+        default:(siempre al final)
+        todas las lineas de cod a ejecutar en caso
+        de coincidir con los case
+    }
+*/  
